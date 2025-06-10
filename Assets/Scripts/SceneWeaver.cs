0fcc3705da3f35309901a5cb9f97fa19db6076d0
DemetrisAnaxagorou <166321736+danaxa01@users.noreply.github.com>
2025-06-10 09:44:20 +0300

    Exit Button in Menu Screen
    -Added exit button in menu screen

--- Assets/Scripts/SceneWeaver.cs
+++ Assets/Scripts/SceneWeaver.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class SceneWeaver : MonoBehaviour
 {
     public void ToMainScreen()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
