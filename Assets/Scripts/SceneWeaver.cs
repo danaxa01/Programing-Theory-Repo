be024a68893c18a5c83863009de11c61bc88a14b
DemetrisAnaxagorou <166321736+danaxa01@users.noreply.github.com>
2025-06-10 09:05:05 +0300

    Title screen initialization
    -Prepared title screen
    -Initialized script for going to the main scene

---
+++ Assets/Scripts/SceneWeaver.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneWeaver : MonoBehaviour
+{
+
+    [SerializeField] private Button start;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ToMainScreen()
+    {
+        
+    }
+}
